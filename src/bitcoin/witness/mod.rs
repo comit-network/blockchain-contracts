69f6974d5b526ff5ed1060374a1605c4f052f7c3
Tobin C. Harding <tobin.harding@coblox.tech>
2019-10-15 19:08:58 +1100

    Improve mediawiki refererced comment

--- src/bitcoin/witness/mod.rs
+++ src/bitcoin/witness/mod.rs
@@ -1,7 +1,7 @@
 // https://github.com/bitcoin/bips/blob/master/bip-0125.mediawiki
 // Wallets that don't want to signal replaceability should use either a
 // max sequence number (0xffffffff) or a sequence number of
-//(0xffffffff-1) when then also want to use locktime;
+// (0xffffffff-1) when then also want to use locktime.
 pub const SEQUENCE_ALLOW_NTIMELOCK_NO_RBF: u32 = 0xFFFF_FFFE;
 #[allow(dead_code)]
 pub const SEQUENCE_DISALLOW_NTIMELOCK_NO_RBF: u32 = 0xFFFF_FFFF;
